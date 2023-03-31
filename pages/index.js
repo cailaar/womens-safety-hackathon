eb7a99c9eeba267f7a65032c770683dbba327087
Clare <clarebudds@yahoo.co.uk>
2023-03-31 15:03:10 +0100

    Adding origin state

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 import Head from 'next/head'
+import React, { useState } from 'react';
 
 export default function Home() {
+  const [origin, setOrigin] = useState('')
+  const [destination, setDestination] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Origin: ${origin}, Destination: ${destination}`);
+  };
+
   return (
     <>
       <Head>
@@ -10,19 +19,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-      <h1>Safe Space</h1>
-    <form className="my-form">
-      <div className="input-wrapper">
-        <label htmlFor="start"></label>
-        <input type="text" id="start" name="start" placeholder='Your location' />
-      </div>
+        <h1>Safe Space</h1>
+        <form onSubmit={handleSubmit} className="my-form">
+          <div className="input-wrapper">
+            <label htmlFor="start"></label>
+            <input type="text" value={origin} onChange={(event) => setOrigin(event.target.value)} id="start" name="start" placeholder='Your location' />
+          </div>
 
-      <div className="input-wrapper">
-        <label htmlFor="end"></label>
-        <input type="text" id="end" name="end" placeholder='Where to?'/>
-      </div>
-      <button>Get route</button>
-    </form>
+          <div className="input-wrapper">
+            <label htmlFor="end"></label>
+            <input type="text" id="end" name="end" placeholder='Where to?' />
+          </div>
+          <button>Get route</button>
+        </form>
 
       </main>
     </>
